9bd79ee3a7d5dc6abc99da22e2c431f37a8b57f1
Shivam Raikundalia <sraikund@meta.com>
2025-11-04 11:41:39 -0800

    Add Cuda Event to Kineto Trace (#1164)
    Summary:
    Pull Request resolved: https://github.com/pytorch/kineto/pull/1164
    
    Adds event for CUDA 13.0+.
    
    For 12.8 they add the event but there is no way of turning on timestamps.
    
    Reviewed By: aaronenyeshi, haibchen
    
    Differential Revision: D84963573
    
    fbshipit-source-id: 3177df34b93bd46e49246f100da920c27e49ac59
    Signed-off-by: Antoni Viros i Martin <aviros@ibm.com>

--- libkineto/src/CuptiActivity.cpp
+++ libkineto/src/CuptiActivity.cpp
@@ -107,6 +107,7 @@ inline int64_t CudaEventActivity::resourceId() const {
 
 inline void CudaEventActivity::log(ActivityLogger& logger) const {
   logger.handleActivity(*this);
+  LOG(INFO) << "CudaEventActivity: " << name() << " " << metadataJson();
 }
 
 inline const std::string CudaEventActivity::metadataJson() const {
